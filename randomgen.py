888e06de7ae46257fcdfcd6c5fa0a7e06b225d45
kcu3 <51998553+kcu3@users.noreply.github.com>
2020-08-15 13:27:12 +0300

    Update randomgen.py

--- randomgen.py
+++ randomgen.py
@@ -6,14 +6,14 @@ def random():
         "jsonrpc": "2.0",
         "method": "generateIntegers",
         "params": {
-            "apiKey": "bd4e7f16-4777-494f-8c30-b1f05b765ace",
+            "apiKey": "#####",
             "n": 2,
             "min": 1,
             "max": 6,
             "replacement": True,
             "base": 10
         },
-        "id": 26657
+        "id": #####
     }
 
     response = requests.post('https://api.random.org/json-rpc/1/invoke', json=req_Data)
